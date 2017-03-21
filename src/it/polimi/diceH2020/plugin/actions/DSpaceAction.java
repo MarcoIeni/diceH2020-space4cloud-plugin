kom-broda/DICE_plugin

aggiunto la pagina di private configuration nel workflow del wizard

src/it/polimi/deib/dspace/actions/DSpaceAction.java
@@ -3,6 +3,7 @@ package it.polimi.deib.dspace.actions;
 
 import it.polimi.deib.dspace.control.DICEWrap;
 import it.polimi.deib.dspace.control.FileManager;
+import it.polimi.deib.dspace.control.PrivateConfiguration;
 import it.polimi.deib.dspace.ui.ConfigurationDialog;
 import it.polimi.deib.dspace.ui.DSpaceWizard;
 
@@ -46,6 +47,7 @@ public class DSpaceAction implements IWorkbenchWindowActionDelegate {
 		System.out.println("Hola "+action.getId());
 		if(action.getId().endsWith("Start")){
 			Configuration.getCurrent().reset();
+			PrivateConfiguration.getCurrent().clear();
 			WizardDialog dialog = new WizardDialog(null, new DSpaceWizard());
 			dialog.open();
 			System.out.println("Starting");

src/it/polimi/deib/dspace/ui/ClassPage.java
@@ -16,6 +16,8 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.List;
 
 import it.polimi.deib.dspace.control.Configuration;
+import it.polimi.deib.dspace.control.PrivateConfiguration;
+import it.polimi.deib.dspace.control.VmClass;
 import utils.JsonDatabase;
 /**
  * Allows user to set parameters for this class.
@@ -32,6 +34,7 @@ public class ClassPage extends WizardPage{
 	private HashMap<String, String> altDtsm;
 	private Button mlProfile ;
 	private String mlPath="";
+	private Button button;
 	protected ClassPage(String title, String description) {
 		super("Browse Files");
 		setTitle(title);
@@ -129,7 +132,7 @@ public class ClassPage extends WizardPage{
 		new Label(container, SWT.NONE);
 		new Label(container, SWT.NONE);
 		new Label(container, SWT.NONE);
-		Button button = new Button(container, SWT.PUSH);
+		button = new Button(container, SWT.PUSH);
 		button.setText("Refresh vm configurations");
 		button.addSelectionListener(new SelectionAdapter(){
 			public void widgetSelected(SelectionEvent e) {
@@ -202,8 +205,14 @@ public class ClassPage extends WizardPage{
 	
 	@Override
 	public boolean canFlipToNextPage(){
+		if(Configuration.getCurrent().getTechnology().contains("Hadoop")){
+			if(!ddsmPath.equals("") && l2.getItemCount() > 0&&!mlPath.equals("")){
+				return true;
+			}else{
+				return false;
+			}
+		}
 		if(!ddsmPath.equals("") && l2.getItemCount() > 0){
-			System.out.println("Can turn");
 			return true;
 		}
 		return false;
@@ -264,6 +273,14 @@ public class ClassPage extends WizardPage{
 	
 	public String getMlPath(){
 		return mlPath;
+	}
+	
+	public void privateCase(){
+		button.setVisible(false);
+		l1.removeAll();
+		for(VmClass vm:PrivateConfiguration.getCurrent().getVmList()){
+			l1.add(vm.getName());
+		}
 	}
 	
 }

src/it/polimi/deib/dspace/ui/DSpaceWizard.java
@@ -6,6 +6,7 @@ import org.eclipse.jface.wizard.Wizard;
 import it.polimi.deib.dspace.control.ClassDesc;
 import it.polimi.deib.dspace.control.Configuration;
 import it.polimi.deib.dspace.control.FileHandler;
+import it.polimi.deib.dspace.control.PrivateConfiguration;
 
 /**
  * Class needed by Eclipse to manage wizards. The core of this class is getNextPage() method.
@@ -49,8 +50,9 @@ public class DSpaceWizard extends Wizard{
 		stPage=new StormDataPage("Set Storm parameter");
 		prConfigPage=new PrivateConfigPage("Set cluster parameters");
 		
-		addPage(prConfigPage);
+		
 		addPage(choice);
+		addPage(prConfigPage);
 		addPage(folPage);
 		addPage(stPage);
 		addPage(hPage);
@@ -68,7 +70,9 @@ public class DSpaceWizard extends Wizard{
 				Configuration.getCurrent().setR(choice.getR());
 				Configuration.getCurrent().setSpsr(choice.getSpsr());
 			}
-			
+			if(Configuration.getCurrent().getIsPrivate()){
+				return prConfigPage;
+			}
 				classp.udpate();
 				classp.setNumClasses(classes);
 				return classp;
@@ -85,7 +89,8 @@ public class DSpaceWizard extends Wizard{
 			}
 			classp.reset();
 			hPage.reset();
-			
+			if(Configuration.getCurrent().getIsPrivate())
+				classp.privateCase();
 			return classp;
 			
 		}
@@ -98,7 +103,9 @@ public class DSpaceWizard extends Wizard{
 			}
 			classp.reset();
 			stPage.reset();
-			
+			if(Configuration.getCurrent().getIsPrivate())
+				classp.privateCase();
+
 			return classp;
 		}
 		
@@ -111,7 +118,7 @@ public class DSpaceWizard extends Wizard{
 			c.setAltDtsm(classp.getAltDtsm());
 			Configuration.getCurrent().getClasses().add(c);
 			if(Configuration.getCurrent().getTechnology().contains("Hadoop")){
-			return hPage;
+				return hPage;
 			}else{
 				return stPage;
 			}
@@ -125,6 +132,21 @@ public class DSpaceWizard extends Wizard{
 			finish = true;
 			return fpage;
 		}
+		
+		if(currentPage==this.prConfigPage){
+			PrivateConfiguration.getCurrent().setPriE(prConfigPage.getCostNode());
+			PrivateConfiguration.getCurrent().setPriM(prConfigPage.getMemForNode());
+			PrivateConfiguration.getCurrent().setPriN(prConfigPage.getNumNodes());
+			PrivateConfiguration.getCurrent().setPriV(prConfigPage.getCpuNode());
+			classp.privateCase();
+			classp.udpate();
+			return classp;
+		}
+		
+		
+		
+		
+		
 		
 		return null;
 	}

MarcoIeni/diceH2020-space4cloud-plugin

src/it/polimi/diceH2020/plugin/actions/DSpaceAction.java
@@ -10,6 +10,7 @@ import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 
 import it.polimi.diceH2020.plugin.control.Configuration;
 import it.polimi.diceH2020.plugin.control.DICEWrap;
+import it.polimi.diceH2020.plugin.control.PrivateConfiguration;
 import it.polimi.diceH2020.plugin.ui.ConfigurationDialog;
 import it.polimi.diceH2020.plugin.ui.DSpaceWizard;
 
@@ -39,6 +40,7 @@ public class DSpaceAction implements IWorkbenchWindowActionDelegate {
 		System.out.println("Hola "+action.getId());
 		if(action.getId().endsWith("Start")){
 			Configuration.getCurrent().reset();
+			PrivateConfiguration.getCurrent().clear();
 			WizardDialog dialog = new WizardDialog(null, new DSpaceWizard());
 			dialog.open();
 			System.out.println("Starting");

src/it/polimi/diceH2020/plugin/ui/ClassPage.java
@@ -16,7 +16,10 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.List;
 
 import it.polimi.diceH2020.plugin.control.Configuration;
+import it.polimi.diceH2020.plugin.control.PrivateConfiguration;
+import it.polimi.diceH2020.plugin.control.VmClass;
 import utils.JsonDatabase;
+
 /**
  * Allows user to set parameters for this class.
  * @author kom
@@ -32,6 +35,7 @@ public class ClassPage extends WizardPage{
 	private HashMap<String, String> altDtsm;
 	private Button mlProfile ;
 	private String mlPath="";
+	private Button button;
 	protected ClassPage(String title, String description) {
 		super("Browse Files");
 		setTitle(title);
@@ -129,7 +133,7 @@ public class ClassPage extends WizardPage{
 		new Label(container, SWT.NONE);
 		new Label(container, SWT.NONE);
 		new Label(container, SWT.NONE);
-		Button button = new Button(container, SWT.PUSH);
+		button = new Button(container, SWT.PUSH);
 		button.setText("Refresh vm configurations");
 		button.addSelectionListener(new SelectionAdapter(){
 			public void widgetSelected(SelectionEvent e) {
@@ -201,8 +205,14 @@ public class ClassPage extends WizardPage{
 	
 	@Override
 	public boolean canFlipToNextPage(){
+		if(Configuration.getCurrent().getTechnology().contains("Hadoop")){
+			if(!ddsmPath.equals("") && l2.getItemCount() > 0&&!mlPath.equals("")){
+				return true;
+			}else{
+				return false;
+			}
+		}
 		if(!ddsmPath.equals("") && l2.getItemCount() > 0){
-			System.out.println("Can turn");
 			return true;
 		}
 		return false;
@@ -264,5 +274,13 @@ public class ClassPage extends WizardPage{
 	
 	public String getMlPath(){
 		return mlPath;
+	}
+	
+	public void privateCase(){
+		button.setVisible(false);
+		l1.removeAll();
+		for(VmClass vm:PrivateConfiguration.getCurrent().getVmList()){
+			l1.add(vm.getName());
+		}
 	}
 }

src/it/polimi/diceH2020/plugin/ui/DSpaceWizard.java
@@ -6,6 +6,7 @@ import org.eclipse.jface.wizard.Wizard;
 import it.polimi.diceH2020.plugin.control.ClassDesc;
 import it.polimi.diceH2020.plugin.control.Configuration;
 import it.polimi.diceH2020.plugin.control.FileHandler;
+import it.polimi.diceH2020.plugin.control.PrivateConfiguration;
 
 /**
  * Class needed by Eclipse to manage wizards. The core of this class is getNextPage() method.
@@ -48,8 +49,9 @@ public class DSpaceWizard extends Wizard{
 		stPage=new StormDataPage("Set Storm parameter");
 		prConfigPage=new PrivateConfigPage("Set cluster parameters");
 		
-		addPage(prConfigPage);
+		
 		addPage(choice);
+		addPage(prConfigPage);
 		addPage(folPage);
 		addPage(stPage);
 		addPage(hPage);
@@ -67,10 +69,12 @@ public class DSpaceWizard extends Wizard{
 				Configuration.getCurrent().setR(choice.getR());
 				Configuration.getCurrent().setSpsr(choice.getSpsr());
 			}
-
-			classp.udpate();
-			classp.setNumClasses(classes);
-			return classp;
+			if(Configuration.getCurrent().getIsPrivate()){
+				return prConfigPage;
+			}
+				classp.udpate();
+				classp.setNumClasses(classes);
+				return classp;
 		}
 
 		if(currentPage==hPage){
@@ -83,6 +87,8 @@ public class DSpaceWizard extends Wizard{
 
 			classp.reset();
 			hPage.reset();
+			if(Configuration.getCurrent().getIsPrivate())
+				classp.privateCase();
 			return classp;
 		}
 
@@ -95,6 +101,8 @@ public class DSpaceWizard extends Wizard{
 			}
 			classp.reset();
 			stPage.reset();
+			if(Configuration.getCurrent().getIsPrivate())
+				classp.privateCase();
 
 			return classp;
 		}
@@ -119,6 +127,16 @@ public class DSpaceWizard extends Wizard{
 			fileHandler.sendFile();
 			finish = true;
 			return fpage;
+		}
+		
+		if(currentPage==this.prConfigPage){
+			PrivateConfiguration.getCurrent().setPriE(prConfigPage.getCostNode());
+			PrivateConfiguration.getCurrent().setPriM(prConfigPage.getMemForNode());
+			PrivateConfiguration.getCurrent().setPriN(prConfigPage.getNumNodes());
+			PrivateConfiguration.getCurrent().setPriV(prConfigPage.getCpuNode());
+			classp.privateCase();
+			classp.udpate();
+			return classp;
 		}
 
 		return null;